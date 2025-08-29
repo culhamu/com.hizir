d8f42d7c16112deebce6055dcbc1c89375f5036d
Mustafa Culha <120686337+culhamu@users.noreply.github.com>
2025-08-29 19:16:04 +0300

    Update vehicleController.js

--- backend/src/controllers/vehicleController.js
+++ backend/src/controllers/vehicleController.js
@@ -1,26 +1,13 @@
-import Vehicle from "../models/Vehicle.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import * as vehicleService from "../services/vehicleService.js";
 
-export const createVehicle = async (req, res, next) => {
-  try {
-    const { brand, model, year, plate } = req.body;
-    const vehicle = await Vehicle.create({
-      brand,
-      model,
-      year,
-      plate,
-      owner: req.user.id
-    });
-    res.status(201).json(vehicle);
-  } catch (err) {
-    next(err);
-  }
-};
+export const createVehicle = asyncHandler(async (req, res) => {
+  const payload = { ...req.body, ownerId: req.user._id };
+  const vehicle = await vehicleService.createVehicle(payload);
+  res.status(201).json(vehicle);
+});
 
-export const getUserVehicles = async (req, res, next) => {
-  try {
-    const vehicles = await Vehicle.find({ owner: req.user.id });
-    res.json(vehicles);
-  } catch (err) {
-    next(err);
-  }
-};
+export const listUserVehicles = asyncHandler(async (req, res) => {
+  const vehicles = await vehicleService.getVehiclesForUser(req.user._id);
+  res.json({ count: vehicles.length, data: vehicles });
+});
