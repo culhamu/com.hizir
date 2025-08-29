a85a2e3d0d251cedce44c368297c7d209956da68
Mustafa Culha <120686337+culhamu@users.noreply.github.com>
2025-08-29 18:55:10 +0300

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("❌ Database connection error:", err.message);
+    process.exit(1);
+  });
